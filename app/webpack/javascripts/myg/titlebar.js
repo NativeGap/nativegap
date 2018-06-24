a8d596fb53459e29c2b2d1ccde7a0bc00b416e8d
Jonas Hübotter <me@jonhue.me>
2018-06-24 18:13:51 +0200

    [#14] Change indentation to 2 spaces (#15)

--- app/webpack/javascripts/myg/titlebar.js
+++ app/webpack/javascripts/myg/titlebar.js
@@ -1,14 +1,14 @@
 import MygTitlebar from 'myg-titlebar';
 
 ['turbolinks:load', 'modalist:render', 'myg-tabs:render'].forEach( (event) => {
-    document.removeEventListener( event, init );
-    document.addEventListener( event, init );
+  document.removeEventListener( event, init );
+  document.addEventListener( event, init );
 })
 function init() {
-    const mygTitlebar = MygTitlebar.init( document.querySelector('.myg-titlebar'), {} );
-    document.addEventListener( 'turbolinks:visit', () => {
-        if (mygTitlebar) {
-            mygTitlebar.welcomer.disappear();
-        }
-    })
+  const mygTitlebar = MygTitlebar.init( document.querySelector('.myg-titlebar'), {} );
+  document.addEventListener( 'turbolinks:visit', () => {
+    if (mygTitlebar) {
+      mygTitlebar.welcomer.disappear();
+    }
+  })
 }
